dcb9f0cef53a2e2bc465261149aebbb9cc7700aa
meg768 <meg768@users.noreply.github.com>
2018-02-02 11:09:36 +0100

    -

--- test.js
+++ test.js
@@ -6,13 +6,13 @@ require('dotenv').config({path: Path.join(process.env.HOME, '.homebridge/.env')}
 
 
 var Gateway = require('./src/gateway.js')
-var gateway = new Gateway({"host": "gw-b8d7af2a9d45"});
+var ikea = new Gateway({"host": "gw-b8d7af2a9d45"});
 
 gateway.connect().then(() => {
 
     console.log('Connected.');
 
-    var device = gateway.devices[65562];
+    var device = ikea.gateway.devices[65562];
     console.log(device.id, device.name);
 
 });
