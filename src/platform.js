5919e78721170fb06716ef379e8297a858d72b83
meg768 <meg768@users.noreply.github.com>
2019-01-18 20:50:05 +0100

    -

--- src/platform.js
+++ src/platform.js
@@ -61,8 +61,8 @@ module.exports = class Platform extends Gateway {
             this.log(JSON.stringify(Ikea.AccessoryTypes));
             this.log('************************************************************************');
 
-            if (device.type === Ikea.AccessoryTypes.plug) {
-                this.log('************************************************************************');
+            if (device.type == 3) {
+                this.log('PLUG!! ************************************************************************');
                 this.devices[device.instanceId] = new Outlet(this, device);
             }
 
